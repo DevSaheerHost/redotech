b24ff97da881e0af9b71d6e595a03a2119d9585a
devsaheerhost <sheerbabu549@gmail.com>
2026-01-22 15:19:34 +0530

    update

--- main.js
+++ main.js
@@ -3,16 +3,7 @@ import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/
 import { getDatabase, ref, set, push, onValue, update } 
 from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 console.log('App running Smother')
-//import {firebaseConfig} from '/firebase/config.js';
-const firebaseConfig = {
-  apiKey: "AIzaSyCDFiJbMP9O66GzqCOZoQr7xtnax-7-Mno",
-  authDomain: "game-b58b0.firebaseapp.com",
-  databaseURL: "https://game-b58b0-default-rtdb.firebaseio.com",
-  projectId: "game-b58b0",
-  storageBucket: "game-b58b0.firebasestorage.app",
-  messagingSenderId: "973101213162",
-  appId: "1:973101213162:web:f424e34ea2782a7ef8fe93"
-};
+import {firebaseConfig} from '/firebase/config.js';
 const $ = s => document.querySelector(s)
 
 
