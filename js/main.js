ff22188f1cdadb5101f2c19ba02325ab94f98f4e
devsaheerhost <sheerbabu549@gmail.com>
2026-01-22 14:43:46 +0530

    update

--- js/main.js
+++ js/main.js
@@ -3,7 +3,16 @@ import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/
 import { getDatabase, ref, set, push, onValue, update } 
 from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-import {firebaseConfig} from '/firebase/config.js';
+//import {firebaseConfig} from '/firebase/config.js';
+const firebaseConfig = {
+  apiKey: "AIzaSyCDFiJbMP9O66GzqCOZoQr7xtnax-7-Mno",
+  authDomain: "game-b58b0.firebaseapp.com",
+  databaseURL: "https://game-b58b0-default-rtdb.firebaseio.com",
+  projectId: "game-b58b0",
+  storageBucket: "game-b58b0.firebasestorage.app",
+  messagingSenderId: "973101213162",
+  appId: "1:973101213162:web:f424e34ea2782a7ef8fe93"
+};
 const $ = s => document.querySelector(s)
 
 
